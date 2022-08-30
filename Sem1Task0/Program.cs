80d9448aada48624a583ee587b388c9603ec61ef
Casey_Ryback <serega126@list.ru>
2022-08-30 23:27:26 +0300

    Seminar1 tasks

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,12 @@
+﻿string? inputline = Console.ReadLine();
+if (inputline!= null)
+{
+    int inputNumber = int.Parse(inputline);
+
+    DateTime d1 = DateTime.Now;
+
+    int outNumber = inputNumber * inputNumber;
+    DateTime d2 = DateTime.Now;
+    Console.WriteLine(d1-d2);
+}
+
