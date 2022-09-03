f857b5efc9739c95df23724c1360ec942a071ef5
Casey_Ryback <serega126@list.ru>
2022-09-03 12:42:20 +0300

    Sem1Task0_Edited

--- Sem1Task0/Program.cs
+++ Sem1Task0/Program.cs
@@ -9,11 +9,20 @@ string? inputline = Console.ReadLine();
 if (inputline != null)
 {
     int inputNumber = int.Parse(inputline);
-    //int outNumber = inputNumber * inputNumber;
-    int outNumber = (int)Math.Pow(inputNumber, 2);
+    int outNumber = 0;
+    
+    DateTime d1 = DateTime.Now;
+    for (int i = 0; i < 10000000; i++)
+    {
+        outNumber = inputNumber * inputNumber;
+    }
+    DateTime d2 = DateTime.Now;
+
+    Console.WriteLine(d2 - d1);
+    //int outNumber = (int)Math.Pow(inputNumber, 2);
 
     //DateTime d1 = DateTime.Now;
     //DateTime d2 = DateTime.Now;
-    Console.WriteLine(outNumber);
+    //Console.WriteLine(outNumber);
 }
 
