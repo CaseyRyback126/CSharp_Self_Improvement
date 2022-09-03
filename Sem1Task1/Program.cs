6b8e1b672e9ad979c995a16b254a2b8ba39bd0fa
Casey_Ryback <serega126@list.ru>
2022-09-03 13:32:28 +0300

    Sem1Task1

--- Sem1Task1/Program.cs
+++ Sem1Task1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//=======================================================================
+// # 1 Напишите программу, которая на вход принимает число и
+// проверяет является ли первое число квадратом второго
+//=======================================================================
+
+
+Console.Write("Введите первое число: ");
+string? inputlineA = Console.ReadLine();
+Console.Write("Введите второе число: ");
+string? inputlineB = Console.ReadLine();
+if (inputlineA != null && inputlineB != null)
+{
+    int inputNumberA = int.Parse(inputlineA);
+    int inputNumberB = int.Parse(inputlineB);
+
+    //bool outResult = (inputNumberA * inputNumberA == inputNumberB);
+    //bool outResult = (inputNumberB / inputNumberA == inputNumberA);
+    bool outResult = (Math.Sqrt(inputNumberB) == inputNumberA);
+
+ 
+    Console.WriteLine(outResult);
+}
+
