a60fe5de7ea61fb75948115647ba3eceee9befd7
Casey_Ryback <serega126@list.ru>
2022-09-03 12:18:52 +0300

    Sem1Task0

--- Sem1Task0/Program.cs
+++ Sem1Task0/Program.cs
@@ -1,12 +1,19 @@
-﻿string? inputline = Console.ReadLine();
-if (inputline!= null)
+﻿//=======================================================================
+// # 0 Напишите программу, которая на вход принимает число и
+// выдает его квадрат (число умноженное на само себя).
+//=======================================================================
+
+
+Console.Write("Введите число: ");
+string? inputline = Console.ReadLine();
+if (inputline != null)
 {
     int inputNumber = int.Parse(inputline);
+    //int outNumber = inputNumber * inputNumber;
+    int outNumber = (int)Math.Pow(inputNumber, 2);
 
-    DateTime d1 = DateTime.Now;
-
-    int outNumber = inputNumber * inputNumber;
-    DateTime d2 = DateTime.Now;
-    Console.WriteLine(d1-d2);
+    //DateTime d1 = DateTime.Now;
+    //DateTime d2 = DateTime.Now;
+    Console.WriteLine(outNumber);
 }
 
