8fe8a204286902a7d710f61f4195e3b29e6aa579
Casey_Ryback <serega126@list.ru>
2022-09-04 22:22:23 +0300

    Sem1Task5

--- Sem1Task5/Program.cs
+++ Sem1Task5/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//=======================================================================
+// # 5 Напишите программу, которая на вход принимает одно число (N),  
+// а на выходе показывает все целые числа в промежутке от -N до N
+//=======================================================================
+
+
+Console.Write("Введите число: ");
+string? inputlineN = Console.ReadLine();
+
+if (inputlineN != null)
+{
+    int inputNumberN = int.Parse(inputlineN);
+    if (inputNumberN > 0)
+    {
+        int startNum = (-1) * inputNumberN;
+        while (startNum < inputNumberN)
+        {
+            Console.Write(startNum + ", ");
+            startNum = startNum + 1;
+        }
+        Console.Write(inputNumberN);
+    }
+}
+
+
