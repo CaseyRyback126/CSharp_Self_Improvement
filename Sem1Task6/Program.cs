6a8894c848130822921d798f65f3af77a8cf100a
Casey_Ryback <serega126@list.ru>
2022-09-05 01:31:43 +0300

    Sem1Task6

---
+++ Sem1Task6/Program.cs
@@ -0,0 +1,20 @@
+﻿// # 6 Напишите программу, которая на вход принимает число и выдаёт, 
+//  является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Введите число: ");
+string? inputline = Console.ReadLine();
+
+if (inputline != null)
+{
+    int inputNumber = int.Parse(inputline);
+    int outResult = inputNumber % 2;
+    if (outResult == 0)
+    {
+       Console.WriteLine("Четное") ;
+    }
+    else
+    {
+        Console.WriteLine("Нечетное");
+    }
+}
+
