689a0be2bff812990600ec31b288df2451e2cb1a
Casey_Ryback <serega126@list.ru>
2022-09-04 22:42:21 +0300

    Sem1Task7

--- Sem1Task7/Program.cs
+++ Sem1Task7/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//=======================================================================
+// # 7 Напишите программу, которая на вход принимает трехзначное число 
+// и на выходе показывает последнюю цифру этого числа.
+//=======================================================================
+
+
+Console.Write("Введите число: ");
+string? inputline = Console.ReadLine();
+
+if (inputline != null)
+{
+    int inputNumber = int.Parse(inputline);
+    if (inputNumber > 99 && inputNumber < 1000)
+    {
+        int lastDigit = inputNumber % 10;
+        Console.Write(lastDigit);
+    }
+    
+}
+
+
