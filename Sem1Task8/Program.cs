56780189836c026346e4cc7bea4f037684e52a20
Casey_Ryback <serega126@list.ru>
2022-09-05 02:37:50 +0300

    Sem1Task8

---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.)
+
+Console.Write("Введите число: ");
+string? inputline = Console.ReadLine();
+
+if (inputline != null)
+{
+    int inputNumber = int.Parse(inputline);
+    int variable = 1;
+    while (variable < inputNumber)
+    {
+        if (variable % 2 == 0)
+        {
+            Console.Write(variable + ", ");
+        }
+        variable = variable + 1;
+    }
+    if (inputNumber % 2 == 0)
+    {
+        Console.WriteLine(inputNumber);
+    }
+
+}
