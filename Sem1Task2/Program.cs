8097c975d3162eb68a43943e760a8dd5a821cfa9
Casey_Ryback <serega126@list.ru>
2022-09-05 00:35:50 +0300

    Sem1Task2

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая на вход принимает два числа 
+// и выдаёт, какое число большее, а какое меньшее.
+
+
+//считываем данные
+Console.Write("Введите первое число: ");
+string? inputLineA = Console.ReadLine();
+Console.Write("Введите второе число: ");
+string? inputLineB = Console.ReadLine();
+//проверка на пустое значение
+if (inputLineA != null && inputLineB != null)
+{
+    int inputNumA = int.Parse(inputLineA);
+    int inputNumB = int.Parse(inputLineB);
+    //сравниваем числа и выводим результат
+    if (inputNumA > inputNumB)
+    {
+        Console.WriteLine("Первое число больше второго");
+
+    }
+    else
+    {
+        Console.WriteLine("Второе число больше первого");
+    }
+
+}
