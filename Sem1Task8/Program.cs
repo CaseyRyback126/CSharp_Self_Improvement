2fb7edd3ad06be7811ed55952cbf8a3b438891c9
Casey_Ryback <serega126@list.ru>
2022-09-05 03:18:01 +0300

    Sem1Task8

--- Sem1Task8/Program.cs
+++ Sem1Task8/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Напишите программу, которая на вход принимает число (N), 
-// а на выходе показывает все чётные числа от 1 до N.)
+// а на выходе показывает все чётные числа от 1 до N.
 
 Console.Write("Введите число: ");
 string? inputline = Console.ReadLine();
@@ -7,18 +7,13 @@ string? inputline = Console.ReadLine();
 if (inputline != null)
 {
     int inputNumber = int.Parse(inputline);
-    int variable = 1;
-    while (variable < inputNumber)
+    int variable = 2;
+    while (variable <= inputNumber - 2)
     {
-        if (variable % 2 == 0)
-        {
-            Console.Write(variable + ", ");
-        }
-        variable = variable + 1;
-    }
-    if (inputNumber % 2 == 0)
-    {
-        Console.WriteLine(inputNumber);
+        Console.Write(variable + ", ");
+
+        variable = variable + 2;
     }
 
+    Console.Write(variable);
 }
