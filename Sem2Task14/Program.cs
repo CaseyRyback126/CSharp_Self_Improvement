b613f19f7815319c279cfd581a6ed7dcb8a0ac16
Casey_Ryback <serega126@list.ru>
2022-09-05 19:46:47 +0300

    Sem2Task14

---
+++ Sem2Task14/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет,
+// кратно ли оно одновременно 7 и 23.
+
+
+//Вариант Евгения
+void Usual()
+{
+    Console.Write("Введите число: ");
+    string? inputLineA = Console.ReadLine();
+    if (inputLineA != null)
+    {
+        int inputNumberA = int.Parse(inputLineA);
+
+        if (inputNumberA % 7 == 0 && inputNumberA % 23 == 0)
+        {
+            Console.Write("Число кратно одновременно 7 и 23");
+        }
+        else
+        {
+            Console.Write("Число не кратно одновременно 7 и 23");
+        }
+    }
+}
+
+    // Вар Анатолия
+    void ShortOneLine()
+    {
+
+        string? inputLineA = Console.ReadLine();
+        if (inputLineA != null) Console.Write(((int.Parse(inputLineA) % 7 == 0) && (int.Parse(inputLineA) % 23 == 0)) ? ("Кратно") : ("Не кратно"));
+    }
+
+    ShortOneLine();
+    Usual();
