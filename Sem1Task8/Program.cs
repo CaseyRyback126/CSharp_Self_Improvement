9b7dbbce3107f6462388258c632f7d08b5dd2841
Casey_Ryback <serega126@list.ru>
2022-09-05 04:18:36 +0300

    Sem1Task8

--- Sem1Task8/Program.cs
+++ Sem1Task8/Program.cs
@@ -1,19 +1,21 @@
 ﻿// Напишите программу, которая на вход принимает число (N), 
 // а на выходе показывает все чётные числа от 1 до N.
 
+//считываем данные
 Console.Write("Введите число: ");
 string? inputline = Console.ReadLine();
 
+//проверка на пустое значение
 if (inputline != null)
 {
-    int inputNumber = int.Parse(inputline);
-    int variable = 2;
-    while (variable <= inputNumber - 2)
+    int inputNumber = int.Parse(inputline);// присваиваем вещественное значение
+    int variable = 2;//вводим переменную
+    while (variable <= inputNumber - 2)//выполняется цикл, до достижения заданных условий 
     {
-        Console.Write(variable + ", ");
+        Console.Write(variable + ", ");//выводятся промежуточные значения
 
-        variable = variable + 2;
+        variable = variable + 2;//увеличиваем переменную на 2
     }
 
-    Console.Write(variable);
+    Console.Write(variable);//выход из цикла и вывод окончательного результата
 }
