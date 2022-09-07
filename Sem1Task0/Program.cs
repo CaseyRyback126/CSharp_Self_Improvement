17cc8896d67cba5dc654ac08c957be614b4eb2f6
Casey_Ryback <serega126@list.ru>
2022-09-07 20:19:47 +0300

    Sem0Task0

--- Sem1Task0/Program.cs
+++ Sem1Task0/Program.cs
@@ -4,25 +4,26 @@
 //=======================================================================
 
 
-Console.Write("Введите число: ");
-string? inputline = Console.ReadLine();
-if (inputline != null)
-{
-    int inputNumber = int.Parse(inputline);
-    int outNumber = 0;
+// Console.Write("Введите число: ");
+// string? inputline = Console.ReadLine();
+// if (inputline != null)
+// {
+//     int inputNumber = int.Parse(inputline);
+//     int outNumber = 0;
     
-    DateTime d1 = DateTime.Now;
-    for (int i = 0; i < 10000000; i++)
-    {
-        outNumber = inputNumber * inputNumber;
-    }
-    DateTime d2 = DateTime.Now;
+//     DateTime d1 = DateTime.Now;
+//     for (int i = 0; i < 10000000; i++)
+//     {
+//         outNumber = inputNumber * inputNumber;
+//     }
+//     DateTime d2 = DateTime.Now;
 
-    Console.WriteLine(d2 - d1);
+//     Console.WriteLine(d2 - d1);
     //int outNumber = (int)Math.Pow(inputNumber, 2);
 
     //DateTime d1 = DateTime.Now;
     //DateTime d2 = DateTime.Now;
     //Console.WriteLine(outNumber);
-}
+// }
 
+ 
