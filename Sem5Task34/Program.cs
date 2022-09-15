2254b25892a986bd6b66f0a2f125bdb03ae6d549
Casey_Ryback <serega126@list.ru>
2022-09-16 01:39:55 +0300

    sem5tasks34

---
+++ Sem5Task34/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+int[] FillArray(int arrLength, int downBorder, int topBorder)
+{
+    //Генератор случайных чисел
+    Random numSintezator = new Random();
+    //Создаем массив
+    int[] arr = new int[arrLength];
+    //Тест границ
+    if (downBorder < topBorder)
+    {
+        //Заполняем массив
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = numSintezator.Next(downBorder, topBorder + 1);
+        }
+    }
+    //Возвращаем результат
+    return arr;
+}
+
+
+int CounterEven(int[] arr)
+{
+    int countEven = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) countEven++;
+    }
+    return countEven;
+}
+
+void Print1DArray(int[] array)
+{
+    Console.WriteLine("[{0}]", string.Join(", ", array));
+}
+
+void PrintCountEven(int line)
+{
+    Console.WriteLine(line);
+}
+
+int[] array = FillArray(12, 100, 999);
+Print1DArray(array);
+PrintCountEven(CounterEven(array));
