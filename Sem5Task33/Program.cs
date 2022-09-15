871aa8d9c3b83ca838716ecc885cbb09f25bd3cb
Casey_Ryback <serega126@list.ru>
2022-09-15 23:32:22 +0300

    sem5tasks31_32_33_35_37

---
+++ Sem5Task33/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте массив. Напишите программу, которая
+//определяет, присутствует ли заданное число в
+//массиве.
+
+// Универсальный метод генерации и заполнение массива
+int[] FillArray(int length, int topBorder, int downBorder)
+{
+    Random rand = new Random();
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(topBorder, downBorder + 1);
+    }
+    return array;
+}
+
+// определяет является ли value элементом массива
+bool Contains(int[] array, int value)
+{
+    foreach (int item in array)
+    {
+        if (item == value)
+            return true;
+    }
+    return true;
+}
+// вывод массив
+void Print1DArray(int[] array)
+{
+    Console.WriteLine("[{0}]", string.Join(", ", array));
+}
+
+// вывод ответа
+void PrintAnswer(bool answer)
+{
+    Console.WriteLine(answer ? "Да" : "Нет");
+}
+
+
+int[] array = FillArray(10, 0, 10);
+Print1DArray(array);
+PrintAnswer(Contains(array, 8));
