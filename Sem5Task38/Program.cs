ce692ef4cf2b41692d447e4eb6fbe87dc89222e7
Casey_Ryback <serega126@list.ru>
2022-09-20 00:55:58 +0300

    sem5tasks36_38

---
+++ Sem5Task38/Program.cs
@@ -0,0 +1,47 @@
+﻿// // №38 Задайте массив вещественных чисел. Найдите разницу между максимальным и 
+// минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+// [2 0,4 9 7,2 78] -> 77,6
+// * Отсортируйте массив методом вставки и методом подсчета, а затем найдите 
+// разницу между первым и последним элементом. Для задачи со звездочкой 
+// использовать заполнение массива целыми числами.
+
+
+// Генерация случайного массива.
+double[] GenArr(int arrLen, double minValue, double maxValue)
+{
+    double[] arr = new double[arrLen];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arrLen; i++)
+    {
+        arr[i] = rnd.NextDouble() * 100 - 10;
+    }
+    return arr;
+}
+//Вычисление разницы значений.
+double MaxMinDifference(double[] arr)
+{
+    double max = int.MinValue;
+    double min = int.MaxValue;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > max) max = arr[i];
+        if (arr[i] < min) min = arr[i];
+    }
+    return (max - min);
+}
+//Вывод массива.
+void PrintArray(double[] array)
+{
+    Console.WriteLine("[{0:N2}]", string.Join(", ", array));
+}
+//Вывод результата.
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+double[] range = GenArr(15, 10, 100);
+PrintArray(range);
+PrintResult("Разница между максимальным и мнимальным элементом массива равна: " + MaxMinDifference(range));
