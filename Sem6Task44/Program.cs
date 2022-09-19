60f673e2c7f508898372c9dffce952fbdedfbbb5
Casey_Ryback <serega126@list.ru>
2022-09-20 01:21:57 +0300

    sem5tasks39_40_42_44_45

---
+++ Sem6Task44/Program.cs
@@ -0,0 +1,46 @@
+﻿//Не используя рекурсию, выведите первые N чисел
+//Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+ 
+// Метод считывания данных пользователя
+int ReadData(string line)
+{
+    // Выводим сообщение
+    Console.Write(line);
+    // Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    // Возвращаем значение
+    return number;
+}
+
+// Метод, печатает одномерный массив
+void Print1DArray(int[] arr)
+{
+    string arrString = "";
+
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        arrString += arr[i] + ", ";
+    }
+
+    arrString += arr[arr.Length - 1];
+    Console.WriteLine(arrString);
+}
+
+int[] Fibonacci(int number)
+{
+    int[] fibonacciArr = new int[number];
+    fibonacciArr[1] = 1;
+
+    for (int i = 2; i < number; i++)
+    {
+        fibonacciArr[i] = fibonacciArr[i - 2] + fibonacciArr[i - 1];
+    }
+
+    return fibonacciArr;
+}
+
+int inputNumber = ReadData("Введите длину последовательности: ");
+
+Console.WriteLine($"Последовательность Фибоначчи длиной {inputNumber}:");
+Print1DArray(Fibonacci(inputNumber));
+
---
+++ Sem6Task45/Program.cs
@@ -0,0 +1,56 @@
+﻿// Напишите программу, которая будет создавать
+//копию заданного одномерного массива с помощью
+//поэлементного копирования.
+
+
+int length = ReadData("Задайте длину массива: ");
+int minValue = ReadData("Задайте минимальное значение: ");
+int maxValue = ReadData("Задайте максимальное значение: ");
+
+int[] array = GenArr(length, minValue, maxValue);
+Print1DArray(array);
+PrintResult("Копия массива");
+Print1DArray(MakeArrayCopy(array));
+
+
+// Получение данных от пользователя и преобразование в число
+int ReadData(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+int[] GenArr(int arrLen, int minValue, int maxValue)
+{
+    int[] arr = new int[arrLen];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arrLen; i++)
+    {
+        arr[i] = rnd.Next(10, 100);
+    }
+    return arr;
+}
+// Вывод результата в консоль
+void PrintResult(string data)
+{
+    Console.WriteLine(data);
+}
+
+// Вывод массива в консоль
+void Print1DArray(int[] arr)
+{
+    Console.WriteLine($"Массив: [{string.Join(", ", arr)}]");
+}
+//Создание копии массива.
+int[] MakeArrayCopy(int[] arr)
+{
+    int[] rev = new int[length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        rev[i] = arr[i];
+    }
+    return rev;
+}
+
